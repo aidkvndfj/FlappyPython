75d59198fe2e63e961b9bbb09f270265e2e04a38
aidkvndfj <eric8@bell.net>
2019-04-16 14:07:39 -0400

    2nd pipe spawns working

--- Game.py
+++ Game.py
@@ -13,7 +13,7 @@ sys.path.insert(0, 'Classes')
 from BirdClass import *
 from PipeClass import *
 
-WIDTH = 300
+WIDTH = 350
 HEIGHT = 500
 FPS = 30
 
@@ -42,9 +42,13 @@ pipes.add(pipe, pipe2) #add pipes to pipe group
 allSprites.add(pipe, pipe2, bird) # Add pipes and bird to all sprites
 
 #~~~~~~~~~ Functions ~~~~~~~~#
-def ReSpawn():
-    pipe.respawn(0)
-    pipe2.respawn(pipe.rect.bottom + 100)
+def ReSpawn(group):
+    if (group == 1):
+        pipe.respawn(0)
+        pipe2.respawn(pipe.rect.bottom + 100)
+    if (group == 2):
+        pipe3.respawn(0)
+        pipe4.respawn(pipe3.rect.bottom + 100)
 
 #~~~~~~ Main Game Loop ~~~~~~#
 running = True
@@ -54,9 +58,11 @@ while (running):
     if (bird.rect.bottom < -50 or bird.rect.top > HEIGHT):
         running = False
 
-    if (pipe.rect.right < WIDTH / 2 and secondSpawn == False):
+    if (pipe.rect.centerx < WIDTH / 2 and secondSpawn == False):
         pipe3 = Pipe(WIDTH, HEIGHT, "MASTER", 0)
         pipe4 = Pipe(WIDTH, HEIGHT, "FOLLOWER", pipe3.rect.bottom + 100)
+        pipes.add(pipe3, pipe4)
+        allSprites.add(pipe3, pipe4)
         secondSpawn = True
 
     for event in pygame.event.get():
@@ -68,7 +74,10 @@ while (running):
                 bird.jump()
 
     if (pipe.rect.right <= 0):
-        ReSpawn()
+        ReSpawn(1)
+    if (secondSpawn == True):
+        if (pipe3.rect.right <= 0):
+            ReSpawn(2)
 
     allSprites.update()
     # print("run")
